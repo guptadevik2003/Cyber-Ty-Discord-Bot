a27ae0870e8a748ba1b21b14c9d31145a116126a
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-01-24 23:25:47 +0530

    added Live Status

--- events/ready.js
+++ events/ready.js
@@ -3,53 +3,7 @@
 
 //         
 
-//         // cyberty-ping Channel Maple Club
-//         const pingChannel = client.channels.cache.get('928648058458218606')
-//         const editMessage = await pingChannel.messages.fetch('928679627298308146')
-//         const infoMessage = await pingChannel.messages.fetch('928679978613235803')
-//         const botOwner = `Devik_NotFound#2695`
-//         const cdSrt = `\`\`\`yml\n`
-//         const cdEnd = `\n\`\`\``
-//         const infoEmoji = `<:info:928697973947392051>`
-//         setInterval( async () => {
-
-//             const editStart = Date.now()
-//             await editMessage.edit({ content: `<a:cat_weewoo:864796595689488404>` })
-//             const editEnd = Date.now()
-
-//             editMessage.edit({ content: `<a:loading:928692093256343653>` })
-
-//             const editPing = `${editEnd - editStart}ms`
-            
-//             const discordJS = dependencies['discord.js'].replace('^', 'v')
-//             const erelaJS = dependencies['erela.js'].replace('^', 'v')
-//             const nodeJS = `v${process.versions.node}`
-//             const memUsage = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
-//             const clientUptime = await client.MSFormatter(client.uptime)
-//             const clientTag = client.user.tag
-//             const apiPing = `${client.ws.ping}ms`
-//             const servers = client.guilds.cache
-//             const channels = client.channels.cache.size
-//             var users = 0
-//             for (const s of servers) {
-//                 users += s[1].memberCount
-//             }
-
-//             const infoEmbed = new MessageEmbed()
-//                 .setColor(`#00ffff`)
-//                 .setTitle(`${client.user.username} Live Stats`)
-//                 .setThumbnail(client.user.displayAvatarURL())
-//                 .setTimestamp()
-//                 .setFooter({ text: `Updates Every 69 Seconds`, iconURL: `${client.user.displayAvatarURL()}` })
-//                 .setDescription(`${cdSrt}Name: ${clientTag} [${client.user.id}]\nApi Latency: ${apiPing}\nBot Latency: ${editPing}${cdEnd}`)
-//                 .addField(`${infoEmoji} General Stats`, `${cdSrt}Servers: ${servers.size}\nUsers: ${users}\nChannels: ${channels}${cdEnd}`, true)
-//                 .addField(`${infoEmoji} System Stats`, `${cdSrt}Node.js: ${nodeJS}\nDiscord.js: ${discordJS}\nErela.js: ${erelaJS}${cdEnd}`, true)
-//                 .addField(`${infoEmoji} Other Stats`, `${cdSrt}Uptime: ${clientUptime}\nBot Owner: ${botOwner}\nMemory Usage: ${memUsage}${cdEnd}`, false)
-//                 .addField(`${infoEmoji} Important Links`, `**[Invite Link](${botInviteLink} 'Invite Link')・[Support Server](${supportServerLink} 'Support Server')・[Website](${botWebsite} 'CyberTy | Your Discord Buddy')**`, false)
-
-//             infoMessage.edit({ embeds: [infoEmbed] })
-
-//         }, secToMs(69));
+//         
 
 
 
@@ -100,7 +54,7 @@
 
 
 // New Code
-const { onReadyChannel, botPrefix, botWebsite, supportServerLink, botInviteLink } = require('../config.json')
+const { onReadyChannel, botPrefix, botWebsite, supportServerLink, botInviteLink, liveStatusChannel } = require('../config.json')
 const dashboardSchema = require('../schemas/dashboard')
 const { dependencies } = require('../package.json')
 const { MessageEmbed } = require('discord.js')
@@ -146,6 +100,53 @@ module.exports = {
         function secToMs(sec) {
             return sec * 1000
         }
+
+
+        // Live Status Channel Support Server
+        const liveChannel = client.channels.cache.get(liveStatusChannel)
+        const editMessage = await liveChannel.messages.fetch('935227233839243355')
+        const statusMessage = await liveChannel.messages.fetch('935227235558891664')
+        const botOwner = `Devik_NotFound#2695`
+        const cdSrt = `\`\`\`yml\n`
+        const cdEnd = `\n\`\`\``
+        const infoEmoji = client.infoEmoji
+        setInterval( async () => {
+            
+            const editStart = Date.now()
+            await editMessage.edit({ content: `${client.weeWooEmoji}` })
+            const editEnd = Date.now()
+            editMessage.edit({ content: `${client.loadingEmoji}` })
+            const editPing = `${editEnd - editStart}ms`
+
+            const discordJS = dependencies['discord.js'].replace('^', 'v')
+            const erelaJS = dependencies['erela.js'].replace('^', 'v')
+            const nodeJS = `v${process.versions.node}`
+            const memUsage = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`
+            const clientUptime = await client.MSFormatter(client.uptime)
+            const clientTag = client.user.tag
+            const apiPing = `${client.ws.ping}ms`
+            const servers = client.guilds.cache
+            const channels = client.channels.cache.size
+            var users = 0
+            for (const s of servers) {
+                users += s[1].memberCount
+            }
+
+            const statusEmbed = new MessageEmbed()
+                .setColor(`#00ffff`)
+                .setTitle(`${client.user.username} Live Stats`)
+                .setThumbnail(client.user.displayAvatarURL())
+                .setTimestamp()
+                .setFooter({ text: `Updates Every 69 Seconds`, iconURL: `${client.user.displayAvatarURL()}` })
+                .setDescription(`${cdSrt}Name: ${clientTag} [${client.user.id}]\nApi Latency: ${apiPing}\nBot Latency: ${editPing}${cdEnd}`)
+                .addField(`${infoEmoji} General Stats`, `${cdSrt}Servers: ${servers.size}\nUsers: ${users}\nChannels: ${channels}${cdEnd}`, true)
+                .addField(`${infoEmoji} System Stats`, `${cdSrt}Node.js: ${nodeJS}\nDiscord.js: ${discordJS}\nErela.js: ${erelaJS}${cdEnd}`, true)
+                .addField(`${infoEmoji} Other Stats`, `${cdSrt}Uptime: ${clientUptime}\nBot Owner: ${botOwner}\nMemory Usage: ${memUsage}${cdEnd}`, false)
+                .addField(`${infoEmoji} Important Links`, `**[Invite Link](${botInviteLink} 'Invite Link')・[Support Server](${supportServerLink} 'Support Server')・[Website](${botWebsite} 'CyberTy | Your Discord Buddy')**`, false)
+
+            statusMessage.edit({ embeds: [statusEmbed] })
+
+        }, secToMs(9));
 
     }
 }
--- functions/clientEmojis.js
+++ functions/clientEmojis.js
@@ -4,4 +4,10 @@ module.exports = (client) => {
 
     client.uncheckEmoji = `<:uncheck:852787219482804254>`
 
+    client.infoEmoji = `<:info:928697973947392051>`
+
+    client.loadingEmoji = `<a:loading:928692093256343653>`
+
+    client.weeWooEmoji = `<a:cat_weewoo:864796595689488404>`
+
 }
