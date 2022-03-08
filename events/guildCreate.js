5f0381596c58b1441f5b5b8622350953d7b3c5c2
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-03-08 15:26:26 +0530

    removed system channel message

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -26,28 +26,6 @@ module.exports = {
         }
 
 
-        // Sending message in System Channel
-        const systemChannelId = guild.systemChannelId
-        if (systemChannelId) {
-
-            const systemChannel = client.channels.cache.get(systemChannelId)
-
-            const systemEmbed = new MessageEmbed()
-                .setColor(`#00ffff`)
-                .setTitle(`:tada: Thanks for adding ${client.user.username}`)
-                .setDescription(`**Heyy!** Thank you for inviting ${client.user.username},\n` +
-                                `To start using CyberTy's Chatbot feature,\n` +
-                                `make a channel and run **${botPrefix}setchannel**\n\n` +
-                                `For a list of all commands use **${botPrefix}help**\n\n` +
-                                `Need help? [Join the support server](${supportServerLink} 'Support Server').`)
-                .setThumbnail(`https://media.giphy.com/media/3o7TKMt1VVNkHV2PaE/giphy.gif`)
-                .setFooter({ text: `${client.user.tag}`, iconURL: `${client.user.displayAvatarURL()}` })
-
-            systemChannel.send({ embeds: [systemEmbed] })
-
-        }
-
-
         // Sending Join Leave Message in Cyber Ty - Support Server
         const notifyChannel = client.channels.cache.get(clientServerJoinLeaveNotifyChannel)
         
