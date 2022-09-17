baa6d1a89fde5b10ff5787f717b531e3c6e75d1d
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-09-17 12:47:28 +0530

    Updated PGamerX API for basic chatbot

--- chatbot/basic.js
+++ chatbot/basic.js
@@ -1,55 +1,84 @@
 const fetch = require('node-fetch')
 
+// Old PGamerX Code
+//module.exports.PGamerX = async (client, message) => {
+//    let result
+//    const apiBase = `https://random-stuff-api.p.rapidapi.com/ai`
+//    const query = `?msg=${message.content}` +
+//                  `&id=${message.author.id}` +
+//                  `&bot_name=${client.user.username}` +
+//                  `&bot_gender=male` +
+//                  `&bot_master=Devik_NotFound#2695` +
+//                  `&bot_age=18` +
+//                  `&bot_company=Maple Club` +
+//                  `&bot_birth_year=2003` +
+//                  `&bot_birth_date=11th December, 2003` +
+//                  `&bot_favorite_color=Blue` +
+//                  `&bot_favorite_book=Harry Potter` +
+//                  `&bot_favorite_band=One Direction` +
+//                  `&bot_favorite_artist=Olivia Rodrigo` +
+//                  `&bot_favorite_actress=Emma Mackey` +
+//                  `&bot_favorite_actor=Tom Holland`
+//    const apiURL = apiBase + query
+//    await fetch(apiURL, {
+//        method: 'GET',
+//        headers: {
+//            'Authorization': `${process.env.PGAMERX_APIKEY}`,
+//            'X-RapidAPI-Host': `random-stuff-api.p.rapidapi.com`,
+//            'X-RapidAPI-Key': `${process.env.RAPIDAPI_DEFAULTAPP_KEY}`
+//        }
+//    })
+//    .then(response => response.json())
+//    .then(data => {
+//        if (!data.AIResponse) {
+//            result = { error: 'no_response' }
+//        } else {
+//            result = { response: data.AIResponse }
+//        }
+//    })
+//    .catch(err => {
+//        result = { error: 'catched_error' }
+//    })
+//    return result
+//}
+
+// New PGamerX Code
 module.exports.PGamerX = async (client, message) => {
     let result
-
-    const apiBase = `https://random-stuff-api.p.rapidapi.com/ai`
-    const query = `?msg=${message.content}` +
-                  `&id=${message.author.id}` +
-                  `&bot_name=${client.user.username}` +
-                  `&bot_gender=male` +
-                  `&bot_master=Devik_NotFound#2695` +
-                  `&bot_age=18` +
-                  `&bot_company=Maple Club` +
-                  `&bot_birth_year=2003` +
-                  `&bot_birth_date=11th December, 2003` +
-                  `&bot_favorite_color=Blue` +
-                  `&bot_favorite_book=Harry Potter` +
-                  `&bot_favorite_band=One Direction` +
-                  `&bot_favorite_artist=Olivia Rodrigo` +
-                  `&bot_favorite_actress=Emma Mackey` +
-                  `&bot_favorite_actor=Tom Holland`
+    
+    const apiBase = `https://v6.rsa-api.xyz/ai/response`
+    const query = `?user_id=${message.author.id}` +
+          		  `&message=${message.content}`
     const apiURL = apiBase + query
-
+    
     await fetch(apiURL, {
         method: 'GET',
         headers: {
             'Authorization': `${process.env.PGAMERX_APIKEY}`,
-            'X-RapidAPI-Host': `random-stuff-api.p.rapidapi.com`,
-            'X-RapidAPI-Key': `${process.env.RAPIDAPI_DEFAULTAPP_KEY}`
+            'Content-Type': `application/json`
         }
     })
     .then(response => response.json())
     .then(data => {
-
-        if (!data.AIResponse) {
+        
+        if (!data.warning) {
 
             result = { error: 'no_response' }
-
+            
         } else {
-
-            result = { response: data.AIResponse }
-
+            
+            result = { response: data.message }
+            
         }
-
+        
     })
     .catch(err => {
-
+        
         result = { error: 'catched_error' }
-
+        
     })
-
-    return result
+    
+    return result    
 }
 
 module.exports.MonkeDev = async (client, message) => {
